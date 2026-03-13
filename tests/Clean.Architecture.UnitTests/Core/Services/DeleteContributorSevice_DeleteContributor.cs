af8442f6ebaafa66a4ae5c74646d824d99d7d159
ankush-ajari <91322414+ankush-ajari@users.noreply.github.com>
2026-03-13 12:49:59 +0530

    Remove ShouldFail test from DeleteContributor tests
    Removed the ShouldFail test method from DeleteContributor service tests.

--- tests/Clean.Architecture.UnitTests/Core/Services/DeleteContributorSevice_DeleteContributor.cs
+++ tests/Clean.Architecture.UnitTests/Core/Services/DeleteContributorSevice_DeleteContributor.cs
@@ -24,10 +24,4 @@ public class DeleteContributorService_DeleteContributor
 
     result.Status.ShouldBe(Ardalis.Result.ResultStatus.NotFound);
   }
-
-  [Fact]
-  public void ShouldFail()
-  {
-      var content = File.ReadAllText("C:\\temp\\data.txt");
-  }
 }
